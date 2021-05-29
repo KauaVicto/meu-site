15ae1217c69436c89b59c4a7e8f82819812e48f2
RebbD <kauavictor259@gmail.com>
2021-05-28 22:00:52 -0300

    abrir img do slide OK

--- js/body.js
+++ js/body.js
@@ -4,6 +4,8 @@ let b1 = document.getElementById('b1')
 let b2 = document.getElementById('b2')
 
 let imgs = document.getElementsByClassName('imagens')
+imgs[0].style.height = '250px'
+imgs[1].style.height = '250px'
 
 let numimg = 1
 
@@ -13,7 +15,26 @@ b2.addEventListener('click', passarDireita)
 b1.addEventListener('click', passarEsqueda)
 
 for(let i = 0; i < imgs.length; i++){
-    imgs[i].addEventListener('click', abrirImg(i))
+    imgs[i].addEventListener('click', () =>{
+
+        if(imgs[i].style.height == '250px'){
+            setTimeout(() => {
+                imgs[i].style.height = '893px'
+                imgs[i].style.zIndex = '20'
+                imgs[i].style.left = '-25vw'
+                imgs[i].style.boxShadow = '0px 0px 0px 100vw rgba(0, 0, 0, 0.6)'
+            },10)
+            
+        }
+    })
+    document.addEventListener('click', () => {
+        if(imgs[i].style.height == '893px'){
+            imgs[i].style.height = '250px'
+            imgs[i].style.left = '0'
+            imgs[i].style.zIndex = '2'
+            imgs[i].style.boxShadow = 'none'
+        }
+    })
 }
 
 
@@ -23,6 +44,7 @@ function passarDireita(){
 
         imgs[0].style.opacity = '0'
         imgs[0].style.zIndex = '-2'
+        imgs[0].style.height = '250px'
 
         imgs[1].style.opacity = '1'
         imgs[1].style.zIndex = '2'
@@ -48,6 +70,7 @@ function passarEsqueda(){
 
         imgs[1].style.opacity = '0'
         imgs[1].style.zIndex = '-2'
+        imgs[1].style.height = '250px'
 
         b1.style.backgroundColor = '#ffffff'
         b2.style.backgroundColor = '#ffffff80'
@@ -61,9 +84,9 @@ function passarEsqueda(){
     }
 }
 
-function abrirImg(i){
-    console.log('deu certo '+i)
-}
+// function abrirImg(i){
+//     console.log('deu certo '+i)
+// }
 
 // setInterval(() => {
 //     passarDireita()
